e042a6ba1160f4a565ff10931e8f24a6488ff3b9
fandomfilms <bcoy@digitalh2o.com>
2016-10-19 22:07:01 -0500

    Base Modal Style
    Had to update the button styles and add a border due to modal overall
    style

---
+++ src/js/modals/modals.js
@@ -0,0 +1,47 @@
+
+// Add a document listener
+document.addEventListener('click', function(event){
+  let buttonName = 'dx-modal-btn';
+  let dataset = 'dxModal';
+  let closeName = 'model-close';
+
+  if(event.target.classList.contains(buttonName)) {
+    // Get all elements needed
+    let element = event.target;
+    let targetId = element.dataset[dataset];
+    let modal = document.getElementById(targetId);
+
+    // Add functionality to animate hide and show of modal
+    modal.show = () => {
+      modal.classList.remove('animate-out');
+      modal.classList.add('animate-in');
+    };
+    modal.hide = () => {
+      modal.classList.remove('animate-in');
+      modal.classList.add('animate-out');
+      setTimeout(() => {
+        modal.classList.remove('animate-out');
+      }, 300);
+    };
+
+    // Show modal
+    modal.show();
+
+    // Add event listener for closing
+    // based upon clicking background
+    modal.onclick = (event) => {
+      if(event.target === modal) {
+        modal.hide();
+      }
+    };
+
+    // Add onclick handlers for any model-close buttons
+    let closes = modal.getElementsByClassName(closeName);
+    for (let i = 0; i < closes.length; i++) {
+      closes[i].onclick = () => {
+        modal.hide();
+      };
+    }
+
+  }
+});
