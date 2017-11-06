f8babffc27b38ad35ab640a8eb9d5ef5e8744615
fandomfilms <bcoy@digitalh2o.com>
2017-11-06 09:00:46 -0600

    Animation Time
    Playing more with Greensock

--- src/js/animations/animations.js
+++ src/js/animations/animations.js
@@ -3,25 +3,44 @@
 
 //btn class animations
 
+TweenMax.from(".btn", 0.5, {
+  opacity:0,
+  scale:0,
+  ease:Bounce.easeout
+});
+
+TweenMax.staggerFrom(".btn", 2, {drawSVG:0}, 0.1);
+
+/* Create a timeline */
+  tl = new TimelineMax();
+
+//click events
+
 document.addEventListener("click", function(event){
   let btnClass = 'btn';
   let btnClassPrimary = 'btn-primary';
   let btnClassSecondary = 'btn-secondary';
+  let expandCard = 'expandCard';
 
   if(event.target.classList.contains(btnClassPrimary)) {
 
     var btnAction = document.getElementsByClassName("btn-primary");
-    TweenMax.to(btnAction, 1, {
+    tl.to(btnAction, 1, {
       paddingRight:60,
       delay:0.5,
       timeScale:0.5,
       ease:Power1.easeIn,
       repeat:1,
       yoyo:true,
+      scale:1.1,
+      ease:Linear.easeNone,
       onStart:onStart,
       onUpdate:onUpdate,
       onComplete:onComplete
     });
+
+    //TweenMax.set(btnAction, {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+    //This randomly changes color
 
     function onStart(){
 
@@ -46,6 +65,7 @@ document.addEventListener("click", function(event){
       repeat:1,
       yoyo:true,
       ease:Sine.easeOut,
+      //ease:SlowMo.ease.config(0.5, 8, true),
       onStart:onStart,
       onUpdate:onUpdate,
       onComplete:onComplete
@@ -70,7 +90,108 @@ document.addEventListener("click", function(event){
 
   }
 
+  /*var list = document.getElementsByClassName("btn-frameless");
+    for (var i = 0; i < list.length; i++) {
+    list[i].setAttribute("id", "expandCard" + i);
+  }*/
+
+  if(event.target.id == expandCard) {
+    var expandHover = document.getElementById("expandCard");
+    TweenMax.to(expandHover, 1, {
+      scale:1.5,
+      ease:Linear.easeNone,
+      repeat:1,
+      yoyo:true,
+      onStart:onStart,
+      onUpdate:onUpdate,
+      onComplete:onComplete
+    });
+
+    //TweenMax.set(btnAction, {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+    //This randomly changes color
+
+    function onStart(){
+
+    }
+
+    function onUpdate(){
+
+    }
+
+    function onComplete(){
+
+    }
+
+  }
+
 });
+
+document.addEventListener("mouseover", function(event){
+  let btnClass = 'btn';
+  let btnClassPrimary = 'btn-primary';
+  let btnClassSecondary = 'btn-secondary';
+  let expandCard = 'expandCard';
+
+    if(event.target.classList.contains(btnClassPrimary)) {
+
+      var btnAction = document.getElementsByClassName("btn-primary");
+      TweenMax.to(btnAction, 0.3, {
+        scale:1.02,
+        ease:Linear.easeNone,
+        //repeat:1,
+        //yoyo:true,
+        onStart:onStart,
+        onUpdate:onUpdate,
+        onComplete:onComplete
+      });
+
+      //TweenMax.set(btnAction, {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+      //This randomly changes color
+
+      function onStart(){
+
+      }
+
+      function onUpdate(){
+
+      }
+
+      function onComplete(){
+
+      }
+
+    }
+
+    if(event.target.id == expandCard) {
+    var expandHover = document.getElementById("expandCard");
+    TweenMax.to(expandHover, 1, {
+      scale:1.5,
+      ease:Linear.easeNone,
+      repeat:1,
+      yoyo:true,
+      onStart:onStart,
+      onUpdate:onUpdate,
+      onComplete:onComplete
+    });
+
+    //TweenMax.set(btnAction, {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+    //This randomly changes color
+
+    function onStart(){
+
+    }
+
+    function onUpdate(){
+
+    }
+
+    function onComplete(){
+
+    }
+
+  }
+
+  });
 
 /*
 function press(){
@@ -87,3 +208,43 @@ function spin(){
   TweenMax.to(logo, 1, {x:150, rotation:360, width:50, repeat:5, yoyo:true});
 }
 */
+
+
+document.addEventListener("mouseout", function(event){
+  let btnClass = 'btn';
+  let btnClassPrimary = 'btn-primary';
+  let btnClassSecondary = 'btn-secondary';
+  let expandCard = 'expandCard';
+
+    if(event.target.classList.contains(btnClassPrimary)) {
+
+      var btnAction = document.getElementsByClassName("btn-primary");
+      TweenMax.to(btnAction, 0.15, {
+        scale:1,
+        ease:Linear.easeNone,
+        //repeat:1,
+        //yoyo:true,
+        onStart:onStart,
+        onUpdate:onUpdate,
+        onComplete:onComplete
+      });
+
+      //TweenMax.set(btnAction, {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+      //This randomly changes color
+
+      function onStart(){
+
+      }
+
+      function onUpdate(){
+
+      }
+
+      function onComplete(){
+
+      }
+
+    }
+
+  });
+
